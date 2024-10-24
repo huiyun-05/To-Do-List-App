32a09a5f692aef61c166de99c1960a46772d8c88
USER <USER@DESKTOP-34BISRN>
2024-10-24 11:33:53 +0800

    "This is my first commit."

---
+++ 24004581k/src/week2/L2Q1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2;
+
+/**
+ *
+ * @author USER
+ */
+public class L2Q1 {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+    
+}
